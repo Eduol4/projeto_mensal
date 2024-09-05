1ec258de34d2070b41fe9fa9d2849713fbfac1da
Eduol4 <eduardodrevessu@gmail.com>
2024-09-05 20:09:05 -0300

    Mockbean.

--- mensal3/src/test/java/com/mensal3/mensal3/controllersTest/UsuarioControllerTests.java
+++ mensal3/src/test/java/com/mensal3/mensal3/controllersTest/UsuarioControllerTests.java
@@ -4,14 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import com.mensal3.mensal3.controllers.UsuarioController;
 import com.mensal3.mensal3.entities.UsuarioEntity;
+import com.mensal3.mensal3.repositories.UsuarioRepository;
 
 @SpringBootTest
 public class UsuarioControllerTests {
@@ -19,7 +23,15 @@ public class UsuarioControllerTests {
     @Autowired
     UsuarioController usuarioController;
 
+    @MockBean
+    UsuarioRepository usuarioRepository;
+
+    // Mockito.when(
+    //     usuarioRepository.save(new UsuarioEntity(null, "brabo").thenReturn(null));
+    // )
+
     @Test
+    @DisplayName("Função que testa a listagem de usuários")
     void testeListAllUsuario() {
         ResponseEntity<List<UsuarioEntity>> usuarios = this.usuarioController.listAllUsuario();
         assertEquals(HttpStatus.OK, usuarios.getStatusCode());
