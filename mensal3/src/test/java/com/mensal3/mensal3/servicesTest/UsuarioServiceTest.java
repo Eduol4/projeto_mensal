963865ecc397f5e0cb172fa0fe055519cf409d92
Eduol4 <eduardodrevessu@gmail.com>
2024-09-11 18:33:58 -0300

    brabo

--- mensal3/src/test/java/com/mensal3/mensal3/servicesTest/UsuarioServiceTest.java
+++ mensal3/src/test/java/com/mensal3/mensal3/servicesTest/UsuarioServiceTest.java
@@ -1,5 +1,10 @@
 package com.mensal3.mensal3.servicesTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mensal3.mensal3.entities.UsuarioEntity;
@@ -9,4 +14,10 @@ import com.mensal3.mensal3.services.UsuarioService;
 @SpringBootTest
 public class UsuarioServiceTest {
     
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Test
+    void teste1() {
+    }
 }
